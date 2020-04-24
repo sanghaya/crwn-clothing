ef52b433d8f751553ab8406fb6ff0d1eafe4f182
everest-sangha <sangha@everestventures.co>
2020-04-24 18:40:18 +0800

    updating with shop page, collection preview, and collection item

--- src/App.js
+++ src/App.js
@@ -2,20 +2,15 @@ import React from 'react';
 import './App.css';
 import { Switch, Route } from 'react-router-dom';
 import Homepage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
 function App() {
-
-  const HatsPage = () => (
-    <div>
-      <h1>Hats Page</h1>
-    </div>
-  )
 
   return (
     <div>
       <Switch>
         <Route exact path = '/' component={Homepage}/>
-        <Route path= '/hats' component={HatsPage}/>
+        <Route path= '/shop' component={ShopPage}/>
       </Switch> 
     </div>
 
